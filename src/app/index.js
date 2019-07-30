d65c9125cb855a4e7fe521bee0315f877fa16b26
Katamaranas <ernestas.zidokas@gmail.com>
2019-07-30 22:20:00 +0300

    state management with react-redux on products component

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
 import { Products, Cart, Favorites, PageNotFound, SingleProduct } from './pages';
-import { Layout } from './components';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Layout } from './components';
 import { ROUTES } from '../constants';
 import { useFetch } from './hooks';
 import { toggleArrayItem } from './util';
+import store from './state';
 import './index.scss';
 
 function onError() {
   return 'Ooops! Something went wrong ⚰️';
+}
+
+function onSuccess(payload) {
+  store.dispatch({ type: 'SET_PRODUCTS', payload: payload });
+
+  return payload;
 }
 
 function App() {
@@ -17,6 +25,7 @@ function App() {
 
   const { loading: isLoading, products, error } = useFetch({
     onError,
+    onSuccess,
     src: 'https://boiling-reaches-93648.herokuapp.com/food-shop/products',
     initialState: [],
     dataKey: 'products',
@@ -54,54 +63,59 @@ function App() {
   };
 
   return (
-    <Router>
-      <Layout>
-        <Switch>
-          <Route
-            path={ROUTES.defaultPage}
-            exact
-            render={() => (
-              <Products
-                toggleFavorite={toggleFavorite}
-                addToCart={addToCart}
-                favorites={favorites}
-                products={products}
-                cart={cart}
-                isLoading={isLoading}
-                error={error}
-                removeFromCart={removeFromCart}
-              />
-            )}
-          />
-          <Route path={ROUTES.cart} exact render={() => <Cart cart={cart} products={products} />} />
-          <Route
-            path={ROUTES.favorites}
-            exact
-            render={() => (
-              <Favorites
-                toggleFavorite={toggleFavorite}
-                cart={cart}
-                addToCart={addToCart}
-                favorites={favorites}
-                products={products}
-                removeFromCart={removeFromCart}
-              />
-            )}
-          />
-          <Route
-            path={ROUTES.product}
-            exact
-            render={props => {
-              const { id } = props.match.params;
-              const product = products.find(product => product.id === id);
-              return <SingleProduct {...props} product={product} isLoading={isLoading} />;
-            }}
-          />
-          <Redirect exact from={ROUTES.home} to={ROUTES.defaultPage} />
-          <Route component={PageNotFound} />
-        </Switch>
-      </Layout>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route
+              path={ROUTES.defaultPage}
+              exact
+              render={() => (
+                <Products
+                  toggleFavorite={toggleFavorite}
+                  addToCart={addToCart}
+                  favorites={favorites}
+                  cart={cart}
+                  isLoading={isLoading}
+                  error={error}
+                  removeFromCart={removeFromCart}
+                />
+              )}
+            />
+            <Route
+              path={ROUTES.cart}
+              exact
+              render={() => <Cart cart={cart} products={products} />}
+            />
+            <Route
+              path={ROUTES.favorites}
+              exact
+              render={() => (
+                <Favorites
+                  toggleFavorite={toggleFavorite}
+                  cart={cart}
+                  addToCart={addToCart}
+                  favorites={favorites}
+                  products={products}
+                  removeFromCart={removeFromCart}
+                />
+              )}
+            />
+            <Route
+              path={ROUTES.product}
+              exact
+              render={props => {
+                const { id } = props.match.params;
+                const product = products.find(product => product.id === id);
+                return <SingleProduct {...props} product={product} isLoading={isLoading} />;
+              }}
+            />
+            <Redirect exact from={ROUTES.home} to={ROUTES.defaultPage} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </Layout>
+      </Router>
+    </Provider>
   );
 }
 
--- src/app/pages/Products/index.js
+++ src/app/pages/Products/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Loader, ProductCard } from '../../components';
 import './index.scss';
 
@@ -24,4 +25,10 @@ function Products({ isLoading, error, products = [], favorites, cart, ...restPro
   );
 }
 
-export default Products;
+function mapStateToProps(state) {
+  const { products } = state.shop;
+
+  return { products };
+}
+
+export default connect(mapStateToProps)(Products);
