1767913307ce00df5cce597b4863fb176572687c
Katamaranas <ernestas.zidokas@gmail.com>
2019-09-03 22:04:23 +0300

    Addes first test to the application

---
+++ src/app/util/toggleArrayItem.test.js
@@ -0,0 +1,13 @@
+import toggleArrayItem from './toggleArrayItem';
+
+describe('toggleArrayItem', () => {
+  it('adds item at the end of array when item is not present in array', () => {
+    const result = toggleArrayItem(['a', 'b', 'c'], 'd');
+    expect(result).toEqual(['a', 'b', 'c', 'd']);
+  });
+
+  it('removes item from array when item is present in array', () => {
+    const result = toggleArrayItem(['a', 'b', 'c'], 'b');
+    expect(result).toEqual(['a', 'c']);
+  });
+});
